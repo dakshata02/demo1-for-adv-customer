701559a0ff1362d92b7602b66be5efcdedfa58dc
Dakshata <Dakshata@DESKTOP-EQNBBRG>
2020-12-26 13:46:55 +0530

    commit 5day3

--- advertisement-management-system-rest/src/test/java/com/capgemini/advertisement/adv/rest/test/AdvApplicationTests.java
+++ advertisement-management-system-rest/src/test/java/com/capgemini/advertisement/adv/rest/test/AdvApplicationTests.java
@@ -1,10 +1,10 @@
-package com.capgemini.advertisement.advertisementmanagementsystemrest;
+package com.capgemini.advertisement.adv.rest.test;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class AdvertisementManagementSystemRestApplicationTests {
+class AdvApplicationTests {
 
 	@Test
 	void contextLoads() {
