fbc06e91095eecb7631036a90d04f768a488a4c5
Dakshata <Dakshata@DESKTOP-EQNBBRG>
2020-12-26 13:40:45 +0530

    commit 3 day3

--- advertisement-management-system-rest/src/main/java/com/capgemini/advertisement/adv/rest/AdvRestApplication.java
+++ advertisement-management-system-rest/src/main/java/com/capgemini/advertisement/adv/rest/AdvRestApplication.java
@@ -1,4 +1,4 @@
-package com.capgemini.advertisement.advertisementmanagementsystemrest;
+package com.capgemini.advertisement.adv.rest;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -8,10 +8,10 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 @SpringBootApplication(scanBasePackages = "com.capgemini.advertisement")
 @EntityScan(basePackages = "com.capgemini.advertisement.entity")
 @EnableJpaRepositories(basePackages = "com.capgemini.advertisement.dao")
-public class AdvertisementManagementSystemRestApplication {
+public class AdvRestApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(AdvertisementManagementSystemRestApplication.class, args);
+		SpringApplication.run(AdvRestApplication.class, args);
 	}
 
 }
